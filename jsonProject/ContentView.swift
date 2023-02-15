dc89b3b2a7ea38dde6e5eb632d52922975490b51
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2023-02-15 10:38:46 +0000

    Initial Commit

---
+++ jsonProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  jsonProject
+//
+//  Created by Abdoulaye Aliou SALL on 15/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ jsonProject/jsonProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  jsonProjectApp.swift
+//  jsonProject
+//
+//  Created by Abdoulaye Aliou SALL on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct jsonProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
